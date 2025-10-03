379a1332a45be1ee35f2f4c0b81ec29ed6c5b5d5
Jack Tench <79285604+JackTench@users.noreply.github.com>
2025-10-03 21:07:27 +0100

    Refactor + add launch command for Win32 Content Prep Tool.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,32 @@
+pub mod download;
+pub mod utils;
+
+use std::process::Command;
+
+use crate::utils::get_data_directory;
+
 // Entry point for Tauri.
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![launch_win32_content_prep_tool])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn launch_win32_content_prep_tool() {
+    // Get path of the exe.
+    let mut path = get_data_directory();
+    path.push("Microsoft-Win32-Content-Prep-Tool");
+    path.push("IntuneWinAppUtil.exe");
+
+    // Convert to string for the command line.
+    let exe_path = path.to_str().expect("Invalid path to exe.");
+
+    let _ = Command::new("cmd")
+        .args(["/C", "start", "", exe_path])
+        .status()
+        .expect("Failed to launch Win32 Content Prep Tool.");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,7 @@
 // Prevents additional console window on Windows in release.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod download;
-mod utils;
-
 fn main() {
-    utils::boot();
+    intune_packager_lib::utils::boot();
     intune_packager_lib::run()
 }
