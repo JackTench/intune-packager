9e44f32d807a71d9924c87df308e7e45179ab55f
Jack Tench <79285604+JackTench@users.noreply.github.com>
2025-10-03 19:27:14 +0100

    Download Win32 Content Prep Tool.

---
+++ src-tauri/src/download.rs
@@ -0,0 +1,20 @@
+use git2::Repository;
+
+use crate::utils;
+
+const URL: &str = "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool";
+const REPO: &str = "Microsoft-Win32-Content-Prep-Tool";
+
+pub fn download_win32_content_prep_tool() {
+    // Get data directory to download to.
+    let mut path = utils::get_data_directory();
+    path.push(REPO);
+
+    // Get repository.
+    if !path.exists() {
+        println!("Downloading Win32 Content Prep Tool!");
+        Repository::clone(URL, path).expect("Failed to download the Microsoft tool.");
+    } else {
+        println!("Skipping download!");
+    }
+}
--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -2,9 +2,13 @@ use std::{fs::create_dir_all, path::PathBuf};
 
 use dirs::data_local_dir;
 
+use crate::download::download_win32_content_prep_tool;
+
 pub fn boot() {
     // Call get_data_directory() so it creates the directory on first launch.
     get_data_directory();
+    // Download Win32 Content Prep Tool.
+    download_win32_content_prep_tool();
 }
 
 pub fn get_data_directory() -> PathBuf {
