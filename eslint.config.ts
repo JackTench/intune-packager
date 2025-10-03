873f8ce180f69dbccafd93d14ad3b3b0e019f154
Jack Tench <79285604+JackTench@users.noreply.github.com>
2025-10-03 18:19:44 +0100

    Implement prettier for code formatting.

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,25 +3,27 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   {
     ignores: [
-      "dist/**", 
-      "node_modules/**", 
+      "dist/**",
+      "node_modules/**",
       "src-tauri/**",
       "**/*.config.js",
       "**/*.config.ts",
       "build/**",
-      "public/**"
-    ]
+      "public/**",
+    ],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser }
+    languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ]);
