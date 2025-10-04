253ff062e979bfb86bbd8e5b8c4358bc3c5b5289
Jack Tench <79285604+JackTench@users.noreply.github.com>
2025-10-04 09:44:02 +0100

    Add Tauri dialog plugin.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ use crate::utils::get_data_directory;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![launch_win32_content_prep_tool])
         .run(tauri::generate_context!())
