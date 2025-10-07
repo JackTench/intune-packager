8f61075bce1a9dc5a9ce7411ec85a9a19a3f0236
Jack Tench <79285604+JackTench@users.noreply.github.com>
2025-10-07 09:47:55 +0100

    Tweak frontend.

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,9 @@ export default function App() {
     <div className="min-h-screen bg-linear-to-tr from-gray-700 to-sky-900">
       <AppNav />
       <div className="flex flex-col items-center justify-center gap-4 py-12">
-        <p className="text-white text-center">The body content goes here.</p>
+        <p className="text-white text-center">
+          Select an executable to package for Intune.
+        </p>
         <AppFileSelect
           label="Select file:"
           value={path}
@@ -29,7 +31,7 @@ export default function App() {
           ]}
         />
         <Button className="bg-blue-500" onClick={onSubmit}>
-          Submit
+          Save
         </Button>
         <p className="text-gray-400 text-center">
           Note: All files in the folder containing the selected file will be
