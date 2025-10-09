81648893ecd1eaba483c97e376d8db29295eb744
Jack Tench <79285604+JackTench@users.noreply.github.com>
2025-10-09 22:44:57 +0100

    [refactor] Clean up download.rs a lot.

--- src-tauri/src/download.rs
+++ src-tauri/src/download.rs
@@ -2,19 +2,18 @@ use std::fs::remove_file;
 
 use git2::Repository;
 
-use crate::utils::{self, get_data_directory};
+use crate::utils::get_data_directory;
 
 const URL: &str = "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool";
 const REPO: &str = "Microsoft-Win32-Content-Prep-Tool";
 
 pub fn download_win32_content_prep_tool() -> anyhow::Result<()> {
     // Get data directory to download to.
-    let mut path = utils::get_data_directory()?;
-    path.push(REPO);
+    let path = get_data_directory()?.join(REPO);
 
     // Get repository.
     if !path.exists() {
-        Repository::clone(URL, path).expect("Failed to download the Microsoft tool.");
+        Repository::clone(URL, path)?;
     }
     cleanup_git_repo_files()?;
 
@@ -22,8 +21,7 @@ pub fn download_win32_content_prep_tool() -> anyhow::Result<()> {
 }
 
 fn cleanup_git_repo_files() -> anyhow::Result<()> {
-    let mut folder = get_data_directory()?;
-    folder.push(REPO);
+    let folder = get_data_directory()?.join(REPO);
 
     // Files to remove.
     let files_to_remove = [
